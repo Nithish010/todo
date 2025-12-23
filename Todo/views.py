cc302968aec23a4bb0d68846cc16317adf157749
Nithishkumar R <nithishrajappan@gmail.com>
2025-12-23 21:59:16 +0530

    add task functionality

--- Todo/views.py
+++ Todo/views.py
@@ -1,6 +1,11 @@
 
 from django.shortcuts import render
+from todoapp.models import Task
 
 
 def home(request):
-    return render(request,'home.html')
+    tasks=Task.objects.filter(is_completed=False)
+    context={
+        't1':tasks,
+    }
+    return render(request,'home.html',context)
